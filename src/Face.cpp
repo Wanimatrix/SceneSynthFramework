bdb6d4a67301fc2d6736361f15daeaf582c36acf
Wouter Franken <wouter.franken@gmail.com>
2015-11-23 13:16:18 +0100

    Test connection

--- src/Face.cpp
+++ src/Face.cpp
@@ -3,7 +3,7 @@
 Face::Face(const aiFace &t_face) {
     for (int i = 0; i < t_face.mNumIndices; ++i)
     {
-        m_vertIndices.push_back(t_face.mIndices[i]);
+        m_vertIndices.push_back(t_face.mIndices[i])
     }
 }
 
